81dd2f3dd0afc5fe68c365b2a88b7332e5883f4f
NusanHuh <adityaparama119@gmail.com>
2024-11-15 20:03:20 +0700

    Add files via upload

---
+++ tes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
+const http = require('http');
+const { Server } = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Membuat server PeerJS
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: '/',
+});
+
+app.use('/peerjs', peerServer);
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit('user-connected', userId);
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).emit('user-disconnected', userId);
+    });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
